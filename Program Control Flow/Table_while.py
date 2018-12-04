b74d09d02b55ae4a406c9711cb27e5e3a18709b5
uday <kanth.mallampalli@gmail.com>
2018-12-04 20:47:43 +0530

    Fourth commit while statement

---
+++ Program Control Flow/Table_while.py
@@ -0,0 +1,10 @@
+n=int(input("Ente the table to print"))
+X=10
+while X<=100:
+    print(n,"X",X,"=",n*X)
+    X+=1
+X=int(input("Enter the Strting number of Sequence"))
+Y=int(input("Enter the Ending number of Sequence"))
+while X>Y:
+    print(X)
+    X-=2
---
+++ Program Control Flow/while_loop2.py
@@ -0,0 +1,5 @@
+count=0
+while count<100:
+    print("Count is %d"%count)
+    count+=1
+print("The end of Count",count)
