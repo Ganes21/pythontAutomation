3a6046e5a0b2491df9e9e682121a20d02939e593
uday <kanth.mallampalli@gmail.com>
2018-12-17 21:58:29 +0530

    commit methods

---
+++ Methods/methods_1.py
@@ -0,0 +1,17 @@
+def sum_nums(a,b,c):
+    """
+    Sum of three numbers
+    :param a:
+    :param b:
+    :param c:
+    :return:
+    """
+    print(a+b+c)
+
+sum_nums(4,5,5)
+
+
+
+sum_nums(2,3)
+
+l=[1,2,3]
---
+++ Methods/methods_return.py
@@ -0,0 +1,27 @@
+"""
+a group of code which can perform a specific task
+methods are reusable and can be called when needed in the code
+"""
+def method_return(a,b,c):
+   """
+
+   :param a:
+   :param b:
+   :param c:
+   :return:
+   """
+   return a+b+c
+
+sum1=method_return(1,2,3)
+print(sum1)
+
+def ifMetro(city):
+    l=['sfa','la','nyc']
+    if city in l:
+        return True
+    else:
+        return False
+x=ifMetro("nyc")
+print(x)
+y=ifMetro("hyd")
+print(y)
